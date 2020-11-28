7ff4d909927ba00e90995a49e44fe95e749301ab
Dzmitry Paulavets <pekarman2@gmail.com>
2020-11-28 23:30:00 +0300

    kakaka

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+$config = array(
+	'title' => 'Блог1',
+	'db' => array(
+		'server' => 'pekar.com',
+		'username' => 'mysql',
+		'password' => 'mysql',
+		'name' => 'testdb'
+	);
+);
---
+++ includes/config.php
@@ -0,0 +1,14 @@
+<?php
+
+$config = array(
+	'title' => 'TEST_BLOG',
+	'vk_url' => 'https://vk.com/pekarr',
+	'db' => array(
+		'server' => '127.0.0.1',
+		'username' => 'mysql',
+		'password' => 'mysql',
+		'name' => 'testdb'
+	),
+);
+
+require "db.php";
